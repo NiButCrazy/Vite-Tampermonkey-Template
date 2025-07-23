5ffc436cc688bbd45ec410adc4389506b32353c5
NiButCrazy <3104955349@qq.com>
2025-07-23 22:17:44 +0800

    build: OK了孩子们，研究出来如何支持React19了

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import monkey, { cdn } from 'vite-plugin-monkey'
+import monkey from 'vite-plugin-monkey'
 import { resolve } from 'path'
 
 // 设置使用的浏览器
-process.env.BROWSER = ''
+process.env.BROWSER = 'chrome'
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
@@ -28,19 +28,25 @@ export default defineConfig({
     react(),
     monkey({
       entry: 'src/main.tsx',
+      server: {
+        prefix: name => `开发版：${ name }.user.js`,
+      },
       userscript: {
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'vite-tampermonkey-template',
         description: '一个脚本测试模板',
         author: 'Ni But Crazy',
         match: [
-          '*://www.google.com.hk'
-        ],
+          '*://www.google.com',
+          '*://www.google.com*',  // 这种奇妙的语法油猴不适用，但脚本猫(ScriptCat)可以匹配
+        ]
       },
       build: {
         externalGlobals: {
-          'react': cdn.jsdelivr('React', 'umd/react.production.min.js'),
-          'react-dom': cdn.jsdelivr('ReactDOM', 'umd/react-dom.production.min.js')
+          'react': [ 'React', () =>
+            'https://cdn.jsdelivr.net/npm/umd-react/dist/react.production.min.js' ],
+          'react-dom/client': [ 'ReactDOM', () =>
+            'https://cdn.jsdelivr.net/npm/umd-react/dist/react-dom.production.min.js' ]
         },
       },
     }),
